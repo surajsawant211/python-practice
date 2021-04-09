a0c33455114e7ba08c8fca5a17cb15810c714574
Suraj Sawant <53471115+surajsawant211@users.noreply.github.com>
2021-04-09 11:58:35 +0530

    Update Mumbai_university_cgpaToPercentage_calculator.py

--- Mumbai_university_cgpaToPercentage_calculator.py
+++ Mumbai_university_cgpaToPercentage_calculator.py
@@ -8,9 +8,9 @@ sem7 = float(input('enter your pointer of sem7:'))
 cgpa_avg = (sem3+sem4+sem5+sem6+sem7)/(5.0)
 print('your avg cgpa is:',cgpa_avg)
 if cgpa_avg < 6.75:
-  #have some fun here :p
   print('you are not eligible for placements.') 
-  print("\U0001F923"*4)
+    #have some fun here :p
+    print("\U0001F923"*4)
 
 elif cgpa_avg > 7.0:
   print('multiply avg_cgpa * 7.4 + 12\n',cgpa_avg*7.4 + 12)
